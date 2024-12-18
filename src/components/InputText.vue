b8879a89054fa995de8f9b067fa8679c4bcabc01
RyuSimyeong <rsm0503@gmail.com>
2024-12-18 19:00:29 +0900

    style: 텍스트 지우기 버튼에 맞춰 input padding 조절

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -5,6 +5,10 @@ type InputTextProps = {
 const model = defineModel<string>();
 defineProps<InputTextProps>();
 
+const handleFocus = () => {
+  console.log('focus');
+};
+
 const handleDeleteClick = () => {
   console.log('/');
 };
@@ -15,10 +19,11 @@ const handleDeleteClick = () => {
     {{ label }}
     <div class="relative">
       <input
-        class="border-gray-400 border rounded-md h-10 px-4 hover:border-blue-400 invalid:border-red-500 focus:border-green-400 focus:outline-none w-full"
+        class="border-gray-400 border rounded-md h-10 pl-4 pr-10 hover:border-blue-400 invalid:border-red-500 focus:border-green-400 focus:outline-none w-full"
         type="text"
         autocomplete="off"
         v-model="model"
+        @focus="handleFocus"
       />
       <div
         class="absolute top-[10px] right-[10px] bg-black w-[20px] h-[20px] text-white flex items-center justify-center cursor-pointer rounded-full opacity-50"
